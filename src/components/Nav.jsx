9771bc8fa367802223b8fbc61193b71e0bf7fee6
biswajitkarmakar03 <biswajitkarmakar404@gmail.com>
2025-05-03 01:08:09 +0530

    Built Navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,24 @@
+import React from 'react'
 import { headerLogo } from '../assets/images';
 import { hamburger } from '../assets/icons';
-
-import React from 'react'
+import {navLinks} from '../constants';
 
 const Nav = () => {
   return (
-    <header>
-        <nav>
-            <a href=''>
-                <img src={headerLogo} />
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
+            <a href='/'>
+                <img src={ headerLogo } alt='Logo' width={130} height={29} />
             </a>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                {navLinks.map((item) => (
+                  <li key={item.label}>
+                      <a href={item.href} className=' font-montserrat leading-normal text-lg text-slate-gray' >
+                        {item.label}
+                      </a>
+                  </li>
+                ))}
+            </ul>
         </nav>
     </header>
   )
